d51d98efe7ba6a420c6c306ee0dc4e97eff57490
green <xgreenx9999@gmail.com>
2023-04-13 12:41:08 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+This repository contains all types and constants of the `ink::env::Environment` trait.
---
+++ lib.rs
@@ -0,0 +1,23 @@
+/// The size of the buffer for encoding and decoding.
+pub const BUFFER_SIZE: usize = 1 << 14; // 16 kB
+
+/// The maximum number of supported event topics provided by the runtime.
+///
+/// The value must match the maximum number of supported event topics of the used
+/// runtime.
+pub const MAX_EVENT_TOPICS: usize = 4;
+
+/// The account id type.
+pub type AccountId = [u8; 32];
+
+/// The type of balances.
+pub type Balance = u128;
+
+/// The type of hash.
+pub type Hash = [u8; 32];
+
+/// The type of a timestamp.
+pub type Timestamp = u32;
+
+/// The type of block number.
+pub type BlockNumber = u64;
